991bd8f0652b104279becb75e0b7251cf9e8f481
Giovanni Mosquera <Giovanni2414g@gmail.com>
2021-09-04 22:51:54 -0500

    Login implemented

--- rpc-users-page/Pages/Users/Index.cshtml.cs
+++ rpc-users-page/Pages/Users/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -20,9 +21,22 @@ namespace rpc_users_page.Pages.Users
         }
 
         public IList<User> User { get;set; }
-
+        [BindProperty(SupportsGet = true)]
+        public string Username { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string Password { get; set; }
+        public string UsernameLogged { get; set; }
         public async Task OnGetAsync()
         {
+            if (!string.IsNullOrEmpty(Username) & !string.IsNullOrEmpty(Password))
+            {
+                var UserLogin = from m in _context.User where Username.Contains(Username) & Password.Contains(Password) select m.Username;
+                if(UserLogin != null)
+                {
+                    HttpContext.Session.SetString("Username", UserLogin.First());
+                    UsernameLogged = UserLogin.First();
+                }
+            }
             User = await _context.User.ToListAsync();
         }
     }
--- rpc-users-page/Startup.cs
+++ rpc-users-page/Startup.cs
@@ -34,6 +34,15 @@ namespace rpc_users_page
             {
                 options.Conventions.AddPageRoute("/Users/Index", "");
             });
+
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,6 +65,8 @@ namespace rpc_users_page
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSession();
 
             app.UseEndpoints(endpoints =>
             {
